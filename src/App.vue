00c5bd1c66fac08c335ab3665da4f4a7517fc7c1
Simon-mac <zeroing_one@163.com>
2024-03-10 09:51:56 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    初始化项目
+  </div>
+</template>
+
+<style scoped>
+  #app {
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 2rem;
+    text-align: center;
+  }
+</style>
